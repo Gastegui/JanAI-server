34f158204a8351ed38f784ac991711110482003e
eki.zuzaeta <eki.zuzaeta@alumni.mondragon.edu>
2024-12-10 12:11:30 +0100

    First object controllers created

--- src/main/java/com/pbl/demo/model/food/FoodRepository.java
+++ src/main/java/com/pbl/demo/model/food/FoodRepository.java
@@ -1,9 +1,14 @@
 package com.pbl.demo.model.food;
 
-import java.util.Optional;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FoodRepository extends JpaRepository<Food, Integer>{
+    Optional<Food> findByFoodName(String foodName);
     
+    @Query("SELECT f FROM Food f JOIN f.ingredients hi JOIN hi.ingredient i WHERE i.ingName IN :ingredientNames")
+    List<Food> findFoodsByIngredients(@Param("ingredientNames") List<String> ingredientNames);
 }
