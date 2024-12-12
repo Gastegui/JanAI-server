b965df38bd3059078cae417b7d6e43dc465d0f84
eki.zuzaeta <eki.zuzaeta@alumni.mondragon.edu>
2024-12-12 10:42:24 +0100

    Created possible methods to get user data

--- src/main/java/com/pbl/demo/controller/objects/UserDataController.java
+++ src/main/java/com/pbl/demo/controller/objects/UserDataController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pbl.demo.model.userData.UserData;
 import com.pbl.demo.model.userData.UserDataRepository;
 
+import jakarta.servlet.http.HttpSession;
+
 @RestController
 @RequestMapping("/user")
 public class UserDataController {
@@ -20,7 +23,7 @@ public class UserDataController {
     private UserDataRepository userRepo;
 
     @GetMapping(value = "/{id}", produces = {"application/json", "application/xml"})
-    public ResponseEntity<UserData> getCampaignById(@PathVariable int id){
+    public ResponseEntity<UserData> getUserDataById(@PathVariable int id){
         Optional<UserData> user = userRepo.findById(id);
         if(!user.isPresent()){
             return ResponseEntity.notFound().build();
@@ -28,4 +31,28 @@ public class UserDataController {
             return new ResponseEntity<>(user.get(), HttpStatus.OK);
         }
     }
+
+    //If we save the user in the http session, we can get it from here instead of communicating with the database
+    @GetMapping(produces = {"application/json", "application/xml"})
+    public ResponseEntity<UserData> getUserDataBySession(HttpSession session){
+        Object userObj = session.getAttribute("user");
+        if(userObj instanceof UserData && userObj != null){
+            UserData user = (UserData) userObj;
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    //This is the same but if we decide to use the security context from spring security
+    @GetMapping(produces = {"application/json", "application/xml"})
+    public ResponseEntity<UserData> getUserBySecurityContext(){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        Optional<UserData> user = userRepo.findByUsername(username);
+        if(!user.isPresent()){
+            return ResponseEntity.notFound().build();
+        }else return new ResponseEntity<>(user.get(), HttpStatus.OK);
+    }
+
+
 }
