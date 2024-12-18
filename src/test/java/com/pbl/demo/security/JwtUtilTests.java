b59beb21d7f443c92d40bfe182beddbdd3db2b8e
Lucas Nobrega <105940701+lucas-pdnobrega@users.noreply.github.com>
2024-12-18 12:48:19 +0100

    Update JwtUtilTests.java

--- src/test/java/com/pbl/demo/security/JwtUtilTests.java
+++ src/test/java/com/pbl/demo/security/JwtUtilTests.java
@@ -1,10 +1,14 @@
 package com.pbl.demo.security;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +16,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import io.jsonwebtoken.ExpiredJwtException;
 
 class JwtUtilTests {    
 
@@ -21,7 +27,7 @@ class JwtUtilTests {
 
     @BeforeEach
     void setUp() {
-        jwtUtil = new JwtUtil("an-incredibly-safe-testing-key-with-over-256-bits", 360000);
+        jwtUtil = new JwtUtil("an-incredibly-safe-testing-key-with-over-256-bits", 36000);
         userDetails = new User("username", "password", List.of(new SimpleGrantedAuthority("ROLE_USER")));
         token = jwtUtil.generateToken(userDetails);
     }
@@ -40,8 +46,29 @@ class JwtUtilTests {
     }
 
     @Test
-    void testClaimAccess() {
-        ass
+    void testExtractClaimWithCustomClaimFunction() {
+        List<String> roles = jwtUtil.extractClaim(token, claims -> (List<String>) claims.get("roles"));
+
+        // Verify the custom claim is correctly extracted
+        assertNotNull(roles, "Roles should not be null.");
+        assert(roles.contains("ROLE_USER"));
+    }
+
+    // @Test
+    // void testTokenExpiration() throws InterruptedException {
+    //     Thread.sleep(3000); // Sleep longer than the expiration time
+
+    //     // Validate that the token is expired
+    //     assertThrows(ExpiredJwtException.class, () -> {
+    //         jwtUtil.validateToken(token, userDetails.getUsername());
+    //     }, "Token should expire after timeout."
+    //     );
+    // }
+
+    @Test
+    void testTokenNotExpired() {
+        // Check that the token is still valid before expiration
+        assertTrue(jwtUtil.validateToken(token, userDetails.getUsername()), "Token should not be expired yet.");
     }
 
     @Test
