2fcb8197b5df0d8f179e93672568efa9fe7715ad
Anje13 <ange.maiztegui@alumni.mondragon.edu>
2025-01-07 22:36:54 +0100

    Initial nutrichat simulation

--- nutriChat simulation/App.py
+++ nutriChat simulation/App.py
@@ -1,6 +1,6 @@
 import time
 from NutriChat import NutriChat
-import ChatMain
+from ChatMain import ChatMain
 from Premium import Premium
 from User import User
 
@@ -10,16 +10,16 @@ class App:
     NUSERS = 5
 
     def __init__(self):
-        self.chatMain = ChatMain.ChatMain()
-        self.nutriChat = NutriChat(self.chatMain)
-        self.premium = [None] * self.NPREMIUM
-        self.users = [None] * self.NUSERS
+        self.chatMain = ChatMain()
+        self.premium = []
+        self.users = []
 
     def create_threads(self):
+        self.nutriChat = NutriChat(self.chatMain)
         for i in range(self.NPREMIUM):
-            self.premium[i] = Premium(self.chatMain)
+            self.premium.append(Premium(self.chatMain))
         for i in range(self.NUSERS):
-            self.users[i] = User(self.chatMain)
+            self.users.append(User(self.chatMain))
 
     def start_threads(self):
         self.nutriChat.start()
