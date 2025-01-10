51cc283d72edd7121ce339b83608fd569576fc85
eki.zuzaeta <eki.zuzaeta@alumni.mondragon.edu>
2025-01-10 08:40:29 +0100

    New Ingredients test

--- src/test/java/com/pbl/demo/model/IngredientsTest.java
+++ src/test/java/com/pbl/demo/model/IngredientsTest.java
@@ -1,53 +1,104 @@
 package com.pbl.demo.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-
+import com.pbl.demo.model.foodGroup.FoodGroup;
+import com.pbl.demo.model.ingredients.Ingredients;
 import org.junit.jupiter.api.Test;
 
-import com.pbl.demo.model.ingredients.Ingredients;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class IngredientsTest {
+class IngredientsTest {
+
     @Test
-    void testDefaultConstructor(){
-        Ingredients ing = new Ingredients();
+    void testDefaultConstructor() {
+        // Arrange & Act
+        Ingredients ingredient = new Ingredients();
 
-        assertNotNull(ing);
-        assertEquals(0, ing.getIngredientID());
-        assertNull(ing.getIngName());
+        // Assert
+        assertNotNull(ingredient);
+        assertEquals(0, ingredient.getIngredientID());
+        assertNull(ingredient.getIngName());
+        assertNull(ingredient.getGroupID());
     }
 
     @Test
-    void testConstructorWithAllParameters(){
-        int ingId = 8;
-        String name = "Tomato";
+    void testConstructorWithAllParameters() {
+        // Arrange
+        int ingredientID = 1;
+        String ingName = "Tomato";
+        FoodGroup foodGroup = new FoodGroup();
 
-        Ingredients ing = new Ingredients(ingId, name);
+        // Act
+        Ingredients ingredient = new Ingredients(ingredientID, ingName, foodGroup);
 
-        assertEquals(name, ing.getIngName());
-        assertEquals(ingId, ing.getIngredientID());
+        // Assert
+        assertEquals(ingredientID, ingredient.getIngredientID());
+        assertEquals(ingName, ingredient.getIngName());
+        assertEquals(foodGroup, ingredient.getGroupID());
     }
 
     @Test
-    void testConstructorWithoutId(){
-        String name = "Tomato";
+    void testConstructorWithPartialParameters() {
+        // Arrange
+        String ingName = "Lettuce";
+        FoodGroup foodGroup = new FoodGroup();
 
-        Ingredients ing = new Ingredients(name);
+        // Act
+        Ingredients ingredient = new Ingredients(ingName, foodGroup);
 
-        assertEquals(name, ing.getIngName());
+        // Assert
+        assertEquals(0, ingredient.getIngredientID());
+        assertEquals(ingName, ingredient.getIngName());
+        assertEquals(foodGroup, ingredient.getGroupID());
     }
 
     @Test
-    void testGettersAndSetters(){
-        Ingredients ing = new Ingredients();
-        int ingId = 8;
-        String name = "Tomato";
+    void testSettersAndGetters() {
+        // Arrange
+        Ingredients ingredient = new Ingredients();
+        int ingredientID = 2;
+        String ingName = "Carrot";
+        FoodGroup foodGroup = new FoodGroup();
 
-        ing.setIngName(name);
-        ing.setIngredientID(ingId);
+        // Act
+        ingredient.setIngredientID(ingredientID);
+        ingredient.setIngName(ingName);
+        ingredient.setGroupID(foodGroup);
 
-        assertEquals(name, ing.getIngName());
-        assertEquals(ingId, ing.getIngredientID());
+        // Assert
+        assertEquals(ingredientID, ingredient.getIngredientID());
+        assertEquals(ingName, ingredient.getIngName());
+        assertEquals(foodGroup, ingredient.getGroupID());
     }
+
+    
+    @Test
+    void testSettersWithNullValues() {
+        // Arrange
+        Ingredients ingredient = new Ingredients();
+
+        // Act
+        ingredient.setIngName(null);
+        ingredient.setGroupID(null);
+
+        // Assert
+        assertNull(ingredient.getIngName());
+        assertNull(ingredient.getGroupID());
+    }
+
+
+
+    @Test
+    void testExtremeValues() {
+        // Arrange
+        Ingredients ingredient = new Ingredients();
+        int extremeID = Integer.MAX_VALUE;
+
+        // Act
+        ingredient.setIngredientID(extremeID);
+
+        // Assert
+        assertEquals(extremeID, ingredient.getIngredientID());
+    }
+
+    
 }
